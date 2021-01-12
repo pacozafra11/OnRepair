439fc4de644698a1cf6ba28fbd0540a74c506806
paco <pacozafra11@hotmail.com>
2021-01-12 22:45:55 +0100

    Creo el formulario en Login

--- index.php
+++ index.php
@@ -3,13 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- CSS Propio -->
+    <link rel="stylesheet" href="css/estilos.css">
+
     <title>On Repair - Login</title>
 </head>
-<body>
+<body id="index">
 
 <?php
-    echo "hola";
+    
 ?>
+
+    <section>        
+        <div class="container">
+            <div class="row formLogin align-items-center justify-content-center">
+                <div class="col-7">
+                    {* Formulario html *}
+                    <form name="formLogin" id="formLogin" action="index.php" method="post">
+
+                    </form>                     
+                </div>                   
+            </div>
+        </div>        
+    </section>
+
+
 
 </body>
 </html>
