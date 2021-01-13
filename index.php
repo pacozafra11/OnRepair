9e23a2aaa174da32d38c18a02e76a390fe7c0384
paco <pacozafra11@hotmail.com>
2021-01-13 20:10:35 +0100

    Añado logotipo y campo texto email al formulario, y los script de jQuery, Bootstrap, Popper, paquetes de iconos y script propio al pie de la página

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- CSS Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- CSS Bootstrap 4-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- CSS Propio -->
     <link rel="stylesheet" href="css/estilos.css">
@@ -19,14 +19,28 @@
 
 <?php
     
+    //Declaración de variables
+    $email= $emailErr= "";
 ?>
 
     <section>        
         <div class="container">
             <div class="row formLogin align-items-center justify-content-center">
-                <div class="col-7">
+                <div class="col-7 mt-3">
                     <!-- Formulario html -->
                     <form name="formLogin" id="formLogin" action="index.php" method="post">
+
+                        <div class="form-group col-12 col-xs-12">
+                            <div class="text-center">
+                                <img src="imagenes/loco_centrado.png" class="rounded" alt="Logotipo On Repair">
+                            </div>    
+                        </div>
+
+                        <!-- Campos rellenables de usuario y contraseña -->
+                        <div class="form-group col-12 col-xs-12">
+                            <input type="text" class="form-control" name="email" id="email" placeholder="&#128272;  Email" value="<?php echo $email;?>" autofocus maxlength="20" required> 
+                            <span class="error"><?php echo $emailErr;?></span>    
+                        </div>
 
                     </form>                     
                 </div>                   
@@ -35,6 +49,17 @@
     </section>
 
 
+    <!-- JS script jquery-3, Popper y Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!-- Paquete de iconos -->
+    <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule="" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
+
+    <!-- Script propios para la página -->
+    <script src="js/funciones.js"></script>
 
 </body>
 </html>
