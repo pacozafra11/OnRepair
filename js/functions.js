bf957e6694bc58199e27009242ab6b1b505d58f3
paco <pacozafra11@hotmail.com>
2021-02-07 19:26:02 +0100

    Comit al final del día, varias pruebas en diferentes archivos con las cookies y las sesiones

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,8 @@
 
 $(function() {  //Con esta línea espera el archivo JS a que se cargue toda la página(HTML5 y CSS3) para ser ejecutado.
 
+    //Ocultar y mostrar menú
+    $('#menu').css("width","250px");
 
     $(document).on("click", "#abrir_menu", function(){ 
         $('#menu').css("width","250px");
